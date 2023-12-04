dcdaf19b052a9d54df165929c287994696e8f44f
MutalageJuliet <150666512+MutalageJuliet@users.noreply.github.com>
2023-12-04 13:22:33 +0300

    Update AboutUs.js

--- AboutUs.js
+++ AboutUs.js
@@ -2,3 +2,4 @@
 // // code to respond to the click of about us may be route to an html file aboutus.html
 // alert("hello this is samuelwebapps.com");
 //console.log("This is julie's comment")
+//Samuel which information do we need for each hostel so that i get some information???
