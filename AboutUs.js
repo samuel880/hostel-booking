dfd0a53c0ff231ed2f992a0117e8f344c7a7b357
MutalageJuliet <150666512+MutalageJuliet@users.noreply.github.com>
2023-12-04 13:14:48 +0300

    Update AboutUs.js

--- AboutUs.js
+++ AboutUs.js
@@ -1,3 +1,4 @@
 // console.log("hello this is the about us message");
 // // code to respond to the click of about us may be route to an html file aboutus.html
 // alert("hello this is samuelwebapps.com");
+//console.log("This is julie's comment")
